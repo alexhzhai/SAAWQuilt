5df9ed5de972eaca5c2734f2f20e8e03a00c9b8d
Alex Zhai <alexhezhai@gmail.com>
2021-02-24 22:47:55 -0800

    scripts working

---
+++ scripts.js
@@ -0,0 +1,19 @@
+var images = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+function showImage(imgNum) {
+    document.getElementById("image0-enlarged").style.display = "none";
+
+    var imageId = "image" + imgNum.toString() + "-enlarged";
+    var x = document.getElementById(imageId);
+    x.style.display = "block";
+
+    for (var i = 0; i < images.length; i++) {
+        if (i !== imgNum) {
+            var removeId = "image" + i.toString() + "-enlarged";
+            var y = document.getElementById(removeId);
+            if (y.style.display == "block") {
+                y.style.display = "none";
+            }
+        }
+    }
+}
